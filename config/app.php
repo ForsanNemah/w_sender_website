da0119b902c2def599eb3b9989c322f09b486215
ForsanNemah <forsan20172017@gmail.com>
2023-01-20 03:11:24 +0300

    update url

--- config/app.php
+++ config/app.php
@@ -87,10 +87,10 @@ return [
    
     'action_url' => 'https://script.google.com/macros/s/AKfycbx04QAVpmvPjI_PqXJO6kPCemsJ4TUjuJzKssrt0IrrBqWYHfNGuE7FmY_glXsxrP5K/exec',
     'phn' => '966553907575',
-    'download_link' => 'https://drive.google.com/file/d/1sYmWySthU6rXvA5gzAYb5t3hmGiVg1yU/view?usp=share_link',
+    'download_link' => 'https://mega.nz/folder/DvJhETha#71caCsXW6aNi3iBUwQlOTQ',
 
 
-    'header_video_link' => 'https://www.youtube.com/watch?v=owKEGwN2pCg&feature=emb_imp_woyt',
+    'header_video_link' => 'https://www.youtube.com/embed/?listType=playlist&list=PLo2nZYkmwvnK5WR77LZL22RY1ppcEdIdp',
     'video_list_link' => 'https://www.youtube.com/embed/?listType=playlist&list=PLo2nZYkmwvnK5WR77LZL22RY1ppcEdIdp',
 
 
